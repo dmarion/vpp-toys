e930c17c3623e0873eb991040a9af2ec047aef0f
Damjan Marion <dmarion@me.com>
2020-06-26 16:59:36 +0200

    trexctl.py improvements
    Signed-off-by: Damjan Marion <dmarion@me.com>

--- trex/trexctl.py
+++ trex/trexctl.py
@@ -4,18 +4,13 @@ import os
 import sys
 import signal
 import argparse
+import ipaddress
 
 sys.path.insert(0, os.path.join(os.environ['HOME'], 'src', 'trex-core',
                                 'scripts', 'automation', 'trex_control_plane',
                                 'interactive'))
 
 from trex.stl.api import *
-
-src_ipv4 = ["172.16.0.2", "172.16.1.2"]
-dst_ipv4 = ["172.16.0.1", "172.16.1.1"]
-start_range = ["10.0.0.3", "10.128.0.3"]
-end_range = ["10.127.255.251", "10.255.255.251"]
-
 
 class TRexCtl:
     def handler(self, signum, frame):
@@ -37,42 +32,60 @@ class TRexCtl:
                        dest="mult", default="100%", help="multiplier")
         p.add_argument('-p', '--profile', action='store',
                        help='profile', choices=profiles, default=profiles[0])
+        p.add_argument('--ip-addr', action='store', dest="ip_addr",
+                       help='ip address', default=["172.16.0.2", "172.16.1.2"])
+        p.add_argument('--default-gw', action='store', dest="default_gw",
+                       help='default gateway', default=["172.16.0.1", "172.16.1.1"])
+        p.add_argument('--range-addr', action='store', dest="range_addr",
+                       help='range address', default=['10.0.0.1', '10.128.0.1'])
+        p.add_argument('--range-mask', action='store', dest="range_mask",
+                       help='range mask', default=['0.127.255.248', '0.127.255.248'])
+
         self.args = p.parse_args()
 
     def get_streams(self, direction=0, **kwargs):
         if direction == 0:
-            src_ip = src_ipv4[0]
-            dst_ip = src_ipv4[1]
-            range_src_start = start_range[0]
-            range_src_end = end_range[0]
-            range_dst_start = start_range[1]
-            range_dst_end = end_range[1]
+            s, d = 0, 1
         else:
-            src_ip = src_ipv4[1]
-            dst_ip = src_ipv4[0]
-            range_src_start = start_range[1]
-            range_src_end = end_range[1]
-            range_dst_start = start_range[0]
-            range_dst_end = end_range[0]
+            s, d = 1, 0
+
+        raw = []
+
+        if self.args.profile == "l3-scale-ip4":
+            print("{:15} {}/{}".format("Source:", self.args.range_addr[s],
+                                       self.args.range_mask[s]))
+            print("{:15} {}/{}".format("Destination:", self.args.range_addr[d],
+                                       self.args.range_mask[d]))
+            src_ip = self.args.range_addr[s]
+            dst_ip = self.args.range_addr[d]
+            src_mask = int(ipaddress.IPv4Address(self.args.range_mask[s]))
+            dst_mask = int(ipaddress.IPv4Address(self.args.range_mask[d]))
+            raw = [
+                STLVmFlowVar(name="sip", size=4, op="random",
+                             max_value="255.255.255.255"),
+                STLVmFlowVar(name="dip", size=4, op="random",
+                             max_value="255.255.255.255"),
+                STLVmWrMaskFlowVar(fv_name="sip", pkt_offset="IP.src",
+                                   pkt_cast_size=4, mask=src_mask),
+                STLVmWrMaskFlowVar(fv_name="dip", pkt_offset="IP.dst",
+                                   pkt_cast_size=4, mask=dst_mask),
+                STLVmFixIpv4(offset="IP")
+            ]
+        if self.args.profile == "l3-base-ip4":
+            src_ip = self.args.ip_addr[s]
+            dst_ip = self.args.ip_addr[d]
 
         pkt = Ether()
         pkt = pkt/IP(src=src_ip, dst=dst_ip)
-        pkt = pkt/UDP(dport=2010, sport=2020)
+        pkt = pkt/UDP(dport=1234, sport=1234)
         pad = max(0, self.args.pkt_len - len(pkt)) * 'x'
         pkt = pkt/pad
 
-        if self.args.profile == "l3-scale-ip4":
-            vm = STLVM()
-            vm.var(name="sip", min_value=range_src_start,
-                   max_value=range_src_end, size=4, op="inc", step=8)
-            vm.var(name="dip", min_value=range_dst_start,
-                   max_value=range_dst_end, size=4, op="inc", step=8)
-            vm.write(fv_name="sip", pkt_offset="IP.src")
-            vm.write(fv_name="dip", pkt_offset="IP.dst")
-            vm.fix_chksum()
-            pkt = STLPktBuilder(pkt=pkt, vm=vm)
+        if raw:
+            pkt = STLPktBuilder(pkt=pkt, vm=STLScVmRaw(raw))
         else:
             pkt = STLPktBuilder(pkt=pkt)
+
         return [STLStream(packet=pkt, mode=STLTXCont())]
 
     def run(self):
@@ -102,8 +115,11 @@ class TRexCtl:
 
             for p in ports:
                 print("Setting port {} into L3 mode...".format(p))
-                c.set_l3_mode(
-                    port=p, src_ipv4=src_ipv4[p], dst_ipv4=dst_ipv4[p])
+                print("{:18} {}".format("IP Address:", self.args.ip_addr[p]))
+                print("{:18} {}".format("Default Gateway:",
+                                        self.args.default_gw[p]))
+                c.set_l3_mode(port=p, src_ipv4=self.args.ip_addr[p],
+                              dst_ipv4=self.args.default_gw[p])
 
             print("Resolve ...")
             c.resolve(ports=ports)
